c17fe6fe7c5f115bad1a0a7f44178ed9a12f0129
Little-Doom1273 <nate.smyth2008@gmail.com>
2026-07-26 02:28:40 -0400

    Delete page.js

--- page.js
+++
@@ -1,9 +0,0 @@
-import SportPage from "../../components/SportPage";
-
-export const revalidate = 300;
-
-export const metadata = { title: "Soccer — Crossover" };
-
-export default function SoccerPage() {
-  return <SportPage sportKey="soccer" />;
-}
